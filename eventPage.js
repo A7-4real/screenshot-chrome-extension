137c24214d9f2ea85371ee16c512b47bd06f189e
A7-4real <a7.4real@gmail.com>
2022-04-19 23:50:01 +0530

    add comments

--- eventPage.js
+++ eventPage.js
@@ -1,28 +1,37 @@
 console.log("eventPage loaded");
 
+// creating screenshot context menu
 var contextMenuItem = {
   id: "s",
-  title: "screenshot",
+  title: "SSS-Screenshot",
   contexts: ["all"],
 };
 
 chrome.contextMenus.create(contextMenuItem);
 console.log("context item created");
 
+// addListener listens when user clicks on context menu and check whether id is
+// same as our menuItemId
 chrome.contextMenus.onClicked.addListener(function (clickData) {
   if (clickData.menuItemId == "s") {
     console.log("context item clicked");
 
+    // we to take screenshot of current and active tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      // Capture screenshot of full visible tab
       chrome.tabs.captureVisibleTab(null, null, function (dataUrl) {
         console.log("Tab screenshot initiated");
+
+        // screenshot url
         var screenshotUrl = dataUrl;
         console.log("screenshot url - ", screenshotUrl);
+
+        // send Message to content to content script with screenshotUrl as data
         chrome.tabs.sendMessage(
           tabs[0].id,
           { action: "take_screenshot", data: screenshotUrl },
           function (response) {
-            alert("screenshot done!");
+            console.log("screenshot done!");
           }
         );
       });
